bb101263d17a472af21fced0c4590f2c63be5e75
MatthewAdams1999 <157847166+MatthewAdams1999@users.noreply.github.com>
2026-03-03 18:21:30 -0700

    Created basic user and entrant class outline with userTest

---
+++ turingEventLottery/app/src/main/java/com/example/turing_eventlottery/Entrant.java
@@ -0,0 +1,10 @@
+package com.example.turing_eventlottery;
+
+import java.util.ArrayList;
+
+public class Entrant extends User {
+
+    public Entrant(String deviceId, String contactInfo) {
+        super(deviceId, contactInfo);
+    }
+}
